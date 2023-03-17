eec53a4f9350c64e7c4fd9be55180870af18bfcd
ismailcanaydin <ismailcanaydin@gmail.com>
2023-03-17 19:11:45 +0300

    redux

---
+++ src/redux/contactSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const contactSlice = createSlice({
+    name: 'contactSlice',
+    reducers: {},
+    initialState: {
+        items: [],
+    }
+})
+
+export default contactSlice.reducer
